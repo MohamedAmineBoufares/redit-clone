8bc25e4bed66cd9a504cb0cb03481f071ff7d7e5
Mohamed Amine <medamineboufares@gmail.com>
2022-10-29 11:38:32 +0100

    added login + logout logic with nextauth

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,11 @@ import {
 
 import { LeftSideHeaderIcons } from "../../utils/icons";
 
+import { signIn, signOut, useSession } from "next-auth/react";
+
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -50,13 +54,34 @@ function Header() {
         <Bars3Icon className="icon" />
       </div>
 
-      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer ">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image fill={true} src={ReditLogBold} alt="Reddit logo" />
-        </div>
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image fill={true} src={ReditLogBold} alt="Reddit logo" />
+          </div>
 
-        <p className="text-gray-400">Sign In</p>
-      </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
+        </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image fill={true} src={ReditLogBold} alt="Reddit logo" />
+          </div>
+
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   );
 }
